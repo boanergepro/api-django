88177e1b9ef17b93971920258125a893b5d6ae34
boanergepro <antonycarrizo96@gmail.com>
2018-02-18 16:26:30 -0400

    [MOD] Agregada logica para mostrar el resultado de la consulta pasada por la url

--- inventario/views.py
+++ inventario/views.py
@@ -1,5 +1,14 @@
 # -*- coding: utf-8 -*-
 from django.http import HttpResponse
+import json
+from .models import Codigo, Articulo
 
 def index(request):
-    return HttpResponse('hoal')
+    return HttpResponse('Bienvenido al api de BMKeros')
+
+def codigo(request, codigo_id):
+    consulta = Codigo.objects.get(id=codigo_id)
+    return HttpResponse(consulta)
+
+def articulo(request, articulo_id):
+    return HttpResponse(articulo_id)
