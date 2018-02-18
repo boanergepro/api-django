30a3d60b05785baa8a122948354cc42df296e27a
boanergepro <antonycarrizo96@gmail.com>
2018-02-18 16:28:09 -0400

    [ADD] Agregadas url de los modelos.

--- inventario/urls.py
+++ inventario/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^(?P<articulo_id>[0-9]+)/$', views.articulo, name='articulo'),
+    #/api/inventario/1/codigo
+    url(r'^(?P<codigo_id>[0-9]+)/codigo/$', views.codigo, name='codigo'),
 ]
