4629f910cfe970604b0440d5d4f8065854812e25
boanergepro <antonycarrizo96@gmail.com>
2018-02-19 16:44:33 -0400

    [MOD] Cambiada la logica para que las views retornen la data en formato json.

--- inventario/views.py
+++ inventario/views.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.http import HttpResponse
+from django.core import serializers
 import json
 from .models import Codigo, Articulo
 
@@ -7,8 +8,31 @@ def index(request):
     return HttpResponse('Bienvenido al api de BMKeros')
 
 def codigo(request, codigo_id):
-    consulta = Codigo.objects.get(id=codigo_id)
-    return HttpResponse(consulta)
+    consulta = Codigo.objects.filter(id=codigo_id)
+    data = {}
+    for objeto in consulta:
+        data['id'] = objeto.pk
+        data['codigo'] = objeto.codigo
+        data['significado'] = objeto.significado
+
+    data = json.dumps(data)
+    return HttpResponse(data, content_type='application/json')
+
+def codigo_index(request):
+    consulta = Codigo.objects.all()
+    data = {}
+
+    acum = 0
+    for objeto in consulta:
+        data[acum] = {
+            'id' : objeto.pk,
+            'codigo' : objeto.codigo,
+            'significado' : objeto.significado
+        }
+        acum = + 1
+
+    data = json.dumps(data)
+    return HttpResponse(data, content_type='application/json')
 
 def articulo(request, articulo_id):
     return HttpResponse(articulo_id)
