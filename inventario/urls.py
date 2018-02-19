40e37fbe75eedc4faeb54f4851b63eec78dd9aac
boanergepro <antonycarrizo96@gmail.com>
2018-02-19 16:41:48 -0400

    [MOD] Cambiadas las urls

--- inventario/urls.py
+++ inventario/urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<articulo_id>[0-9]+)/$', views.articulo, name='articulo'),
-    #/api/inventario/1/codigo
-    url(r'^(?P<codigo_id>[0-9]+)/codigo/$', views.codigo, name='codigo'),
+    #/api/inventario/codigo/1
+    url(r'^codigo/(?P<codigo_id>[0-9]+)$', views.codigo, name='codigo'),
+    #/api/inventario/codigo --> todos los registros
+    url(r'codigo/', views.codigo_index, name='codigo'),
 ]
